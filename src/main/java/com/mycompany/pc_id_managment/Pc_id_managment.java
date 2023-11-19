86dc4f5496d87a7b93fc95ab38bd86e3f746d3aa
Cheramilak <saremsoftware@gmsil.com>
2023-11-19 16:35:48 -0500

    first commite

---
+++ src/main/java/com/mycompany/pc_id_managment/Pc_id_managment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pc_id_managment;
+
+/**
+ *
+ * @author root
+ */
+public class Pc_id_managment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
